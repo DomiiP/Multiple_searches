2ad158a244d810baa920efa331acc1baef55ddb0
DomiiP <95188800+DomiiP@users.noreply.github.com>
2023-03-07 21:16:59 +0100

    Made some minor changes

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 >This will be a basic program that will search multiple things at once
 My TODO steps:
-- Simple command program for multiple searches
-- Simple GUI program for multiple searches & some additional features
-- Implemented everything onto a website
-- More features ?
+1. Simple command program for multiple searches
+2.  Simple GUI program for multiple searches & some additional features
+3.  Implemented everything onto a website
+4. More features ?
 
 Changelog:
 - 7.3.2023
-    * Created the whole project (README.md and searches.py)
+    * Created the whole project (_README.md_ and _searches.py_)
     * Created a Simple command program for multiple searches
--- searches_without_parameters.py
+++ searches_without_parameters.py
@@ -1,31 +1,21 @@
 import webbrowser
-import sys
 
-# --- | (1) Simple command program for multiple searches (with parameters)| ---
+# Basic command
+#webbrowser.open("https://google.com")
 
-if __name__ == "__main__":
-    # Words
-    try:
-        words  = sys.argv[1]
-    except Exception as e:
-        print(e)
-    try:
-        before = sys.argv[2]
-    except Exception as e:
-        print(e)
-    try:
-        after  = sys.argv[3]
-    except Exception as e:
-        print(e)
-    print(f'This is a test print of words: {words}, and also of before: {before} and of after: {after}')
-    
-# For easier implementation I will just use Google
-browser = 'google'
+# --- | (1) Simple command program for multiple searches (without program paramaters)| ---
+
+# Variables
+static_before_word = 'Red'
+static_after_word  = ''
+list_of_words = ['Couch','dog', 'cat']
+                          #Google DuckDuckGo Bing
+list_of_enabled_browsers = [True,   False   ,False ]
 # This will not be implemented in command program
 specified_browser = 'chrome'
 
 # Logic behind it
-for word in words:
+for word in list_of_words:
     # Google
     if(list_of_enabled_browsers[0]):
         webbrowser.open(f"https://google.com/search?q={static_before_word}+{word}+{static_after_word}")
