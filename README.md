6b5cbc60f983fd592c302e696107abc0c2ede20d
DomiiP <95188800+DomiiP@users.noreply.github.com>
2023-03-07 20:25:25 +0100

    First commit & push for multiple searches

---
+++ README.md
@@ -0,0 +1,14 @@
+#Multiple searches
+
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+>This will be a basic program that will search multiple things at once
+My TODO steps:
+- Simple command program for multiple searches
+- Simple GUI program for multiple searches & some additional features
+- Implemented everything onto a website
+- More features ?
+
+Changelog:
+- 7.3.2023
+    * Created the whole project (README.md and searches.py)
+    * Created a Simple command program for multiple searches
---
+++ searches.py
@@ -0,0 +1,29 @@
+import webbrowser
+import sys
+
+# Basic command
+#webbrowser.open("https://google.com")
+
+# --- | (1) Simple command program for multiple searches| ---
+
+# Variables
+static_before_word = 'Red'
+static_after_word  = ''
+list_of_words = ['Couch','dog', 'cat']
+                          #Google DuckDuckGo Bing
+list_of_enabled_browsers = [True,   False   ,False ]
+# This will not be implemented in command program
+specified_browser = 'chrome'
+
+# Logic behind it
+
+for word in list_of_words:
+    # Google
+    if(list_of_enabled_browsers[0]):
+        webbrowser.open(f"https://google.com/search?q={static_before_word}+{word}+{static_after_word}")
+    # DuckDuckGo
+    if(list_of_enabled_browsers[1]):
+        webbrowser.open(f"https://duckduckgo.com/?q={static_before_word}+{word}+{static_after_word}")
+    # Bing
+    if(list_of_enabled_browsers[2]):
+        webbrowser.open(f"https://bing.com/search?q=={static_before_word}+{word}+{static_after_word}")
